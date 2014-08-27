46afeece07eedb3b0430d1751851ffb3c46e662a
Yijia <guyijia@gmail.com>
2014-08-27 10:17:49 -0400

    delete float

--- float/reorder.h
+++
@@ -1,7 +0,0 @@
-#ifndef REORDER_H_
-#define REORDER_H_
-float getMax(int round_mode, int op, float* fs, int size);
-float getMin(int round_mode, int op, float* fs, int size);
-float getMaxWithFMA(int round_mode, float* fs1, float* fs2, int size);
-float getMinWithFMA(int round_mode, float* fs1, float* fs2, int size);
-#endif
