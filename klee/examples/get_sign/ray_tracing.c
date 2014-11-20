a17d74be6c505773aae124234a2d9e3c34a007ed
Yijia <guyijia@gmail.com>
2014-11-20 18:06:26 -0500

    ray tracing array

--- klee/examples/get_sign/ray_tracing.c
+++ klee/examples/get_sign/ray_tracing.c
@@ -5,18 +5,18 @@
 #include <klee/klee.h>
 
 int ray_tracing(float *r, float *s) {
-  //s[0] = 17.988571f;
-  //s[1] = 19.597578f;
-  //s[2] = 135.595596f;
+  s[0] = 17.988571f;
+  s[1] = 19.597578f;
+  s[2] = 135.595596f;
   //sx = 0.005782f ;
   //sy = 31.999878f;
   //sz = 0.111744f;
   float A = 0; 
   int i = 0;
-  for(; i< 300; i ++){
+  for(; i< 100; i ++){
     A = A + r[i]*r[i];
   }  
-
+  
   klee_tag_reorderable(&A, 0, 2);
 
   float B1 = s[0]*r[0] + s[1]*r[1] + s[2]*r[2];
@@ -26,14 +26,14 @@ int ray_tracing(float *r, float *s) {
   float C1 = s[0]*s[0]+s[1]*s[1]+s[2]*s[2];
   klee_tag_reorderable(&C1, 0, 2);
   float C =  C1 - 14.128791f;
-  //float D = B*B-4*A*C;  
-  float D = A + B - C ;                
+  float D = B*B-4*A*C;  
+  //float D = A + B - C ;              
   if (D > 0)
     return 0;
 } 
 
 int main() {
-  float r[300], s[3];
+  float r[100], s[3];
   klee_make_symbolic_with_sort(&r, sizeof(r), "r", 8, 32);
   klee_make_symbolic_with_sort(&s, sizeof(s), "s", 8, 32);
   
