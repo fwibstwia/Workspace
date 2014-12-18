5dee96692c5c590739d809ef1b58bf6cb2749c3c
Yijia <guyijia@gmail.com>
2014-12-17 19:12:16 -0500

    mandelbrot example

---
+++ klee/examples/get_sign/mandelbrot.c
@@ -0,0 +1,47 @@
+/**  mandel.c   by Eric R. Weeks   written 9-28-96
+ **weeks@physics.emory.edu
+ **http://www.physics.emory.edu/~weeks/
+ **
+ **  This program is public domain, but this header must be left intact
+ **  and unchanged.
+ **
+ **  to compile:  cc -o mand mandel.c
+ **
+ **/
+
+#include <klee/klee.h>
+
+mandelbrot(double x, double y)
+{
+        double cond;
+        int iteration;
+        //int itermax = 100;              /* how many iterations to do    */
+        //double magnify=1.0;             /* no magnification             */
+  
+        //cx = (((float)hx)/((float)hxres)-0.5)/magnify*3.0-0.7;
+        //cy = (((float)hy)/((float)hyres)-0.5)/magnify*3.0;
+        double cx = -2.19399999971501547;
+	double cy = -1.49399999971501529;
+       
+        for (iteration = 1;iteration < 2;iteration++)  {
+	   double xx1 = y*y + cx;
+	   klee_tag_reorderable(&xx1, 0, 3);
+           double xx = x*x + (-xx1);
+           klee_tag_reorderable(&xx, 0, 3);
+           double y1 = 2.0*x;
+           y = y1*y+cy;
+           klee_tag_reorderable(&y, 0, 3);
+           x = xx;          
+        }       
+        cond = x*x + y*y; 
+        klee_tag_reorderable(&cond, 0, 3);
+	if (cond > 100.0)  
+	  iteration = 999999;	
+}
+
+int main() {
+  double x, y;
+  klee_make_symbolic_with_sort(&x, sizeof(x), "x", 0, 64);
+  klee_make_symbolic_with_sort(&y, sizeof(y), "y", 0, 64);
+  return mandelbrot(x, y);
+} 
